1a0fefa914f33709265de3aa96df470666802ee9
Archit Ghetiya <ghetiyarchit007@gmail.com>
2024-12-01 17:30:10 +0530

    aws lambda

--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -1,4 +1,30 @@
-import { someFunction } from "lambda-function";
+// import { someFunction } from "lambda-function";
 
-console.log("Hello from the Infrastructure package!");
-console.log(someFunction());
+// console.log("Hello from the Infrastructure package!");
+// console.log(someFunction());
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+
+export class LambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Define the Lambda function
+    const lambdaFunction = new lambda.Function(this, "HelloLambda", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("../lambda-function/dist"), // Path to compiled Lambda code
+    });
+
+    // Create an API Gateway to trigger the Lambda function
+    new apigateway.LambdaRestApi(this, "HelloApi", {
+      handler: lambdaFunction,
+    });
+  }
+}
+
+// Define an entry point for the CDK app
+const app = new cdk.App();
+new LambdaStack(app, "LambdaStack");
--- packages/lambda-function/src/index.ts
+++ packages/lambda-function/src/index.ts
@@ -1,3 +1,16 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const name = event.queryStringParameters?.name || "World";
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: `Hello, ${name}` }),
+  };
+};
+
+// Export a utility function for local testing.
 export const someFunction = () => {
   return "Hello from the Lambda function!";
 };

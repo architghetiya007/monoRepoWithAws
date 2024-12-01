ea37534343f26c58807d837c296dfd261230f2ff
Archit Ghetiya <ghetiyarchit007@gmail.com>
2024-12-01 16:21:51 +0530

    lambda function issue still here

---
+++ packages/infrastructure/src/index.ts
@@ -0,0 +1,4 @@
+import { someFunction } from "lambda-function";
+
+console.log("Hello from the Infrastructure package!");
+console.log(someFunction());
---
+++ packages/lambda-function/src/index.ts
@@ -0,0 +1,3 @@
+export const someFunction = () => {
+  return "Hello from the Lambda function!";
+};

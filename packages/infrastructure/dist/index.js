edd837e0f9871f32a5dea865efb9d8df77daf394
Archit Ghetiya <ghetiyarchit007@gmail.com>
2024-12-01 22:04:52 +0530

    remove comment

--- packages/infrastructure/dist/index.js
+++ packages/infrastructure/dist/index.js
@@ -8,21 +8,18 @@ const cdk = require("aws-cdk-lib");
 const lambda = require("aws-cdk-lib/aws-lambda");
 const apigateway = require("aws-cdk-lib/aws-apigateway");
 class LambdaStack extends cdk.Stack {
-    constructor(scope, id, props) {
-        super(scope, id, props);
-        // Define the Lambda function
-        const lambdaFunction = new lambda.Function(this, "HelloLambda", {
-            runtime: lambda.Runtime.NODEJS_16_X,
-            handler: "index.handler",
-            code: lambda.Code.fromAsset("../lambda-function/dist"), // Path to compiled Lambda code
-        });
-        // Create an API Gateway to trigger the Lambda function
-        new apigateway.LambdaRestApi(this, "HelloApi", {
-            handler: lambdaFunction,
-        });
-    }
+  constructor(scope, id, props) {
+    super(scope, id, props);
+    const lambdaFunction = new lambda.Function(this, "HelloLambda", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("../lambda-function/dist"),
+    });
+    new apigateway.LambdaRestApi(this, "HelloApi", {
+      handler: lambdaFunction,
+    });
+  }
 }
 exports.LambdaStack = LambdaStack;
-// Define an entry point for the CDK app
 const app = new cdk.App();
 new LambdaStack(app, "LambdaStack");
--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -1,7 +1,3 @@
-// import { someFunction } from "lambda-function";
-
-// console.log("Hello from the Infrastructure package!");
-// console.log(someFunction());
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
@@ -11,20 +7,17 @@ export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Define the Lambda function
     const lambdaFunction = new lambda.Function(this, "HelloLambda", {
       runtime: lambda.Runtime.NODEJS_16_X,
       handler: "index.handler",
-      code: lambda.Code.fromAsset("../lambda-function/dist"), // Path to compiled Lambda code
+      code: lambda.Code.fromAsset("../lambda-function/dist"),
     });
 
-    // Create an API Gateway to trigger the Lambda function
     new apigateway.LambdaRestApi(this, "HelloApi", {
       handler: lambdaFunction,
     });
   }
 }
 
-// Define an entry point for the CDK app
 const app = new cdk.App();
 new LambdaStack(app, "LambdaStack");

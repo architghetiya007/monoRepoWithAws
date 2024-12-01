fa82cc54ec9cd151f9710554e7a64c974bdc3f3e
Archit Ghetiya <ghetiyarchit007@gmail.com>
2024-12-01 22:42:17 +0530

    lambda change

--- packages/lambda-function/dist/index.js
+++ packages/lambda-function/dist/index.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.someFunction = exports.handler = void 0;
+const swaggerDocument = require("./swagger.json");
 const handler = async (event) => {
+    const path = event.path;
+    if (path === "/docs") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(swaggerDocument),
+            headers: { "Content-Type": "application/json" },
+        };
+    }
     const name = event.queryStringParameters?.name || "World";
     return {
         statusCode: 200,
--- packages/lambda-function/src/index.ts
+++ packages/lambda-function/src/index.ts
@@ -1,8 +1,18 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import * as swaggerDocument from "./swagger.json";
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  const path = event.path;
+
+  if (path === "/docs") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(swaggerDocument),
+      headers: { "Content-Type": "application/json" },
+    };
+  }
   const name = event.queryStringParameters?.name || "World";
   return {
     statusCode: 200,
